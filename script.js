2b622b6611af03a149fd798d6c730ec2f866b1e6
Rachel Lyons <rlyons21@hotmail.com>
2016-07-30 11:48:15 -0500

    changes content when click tab doesnt change tab style yet

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Replace me with your JavaScript!
+window.addEventListener("load", function(){
+	var tabs = document.getElementsByClassName("tabtop");
+	var content = document.getElementsByClassName("tab");
+
+	for(i=0; i<tabs.length; i++){
+		tabs[i].addEventListener("click", function(){
+			var id = this.getAttribute("id");
+			var t = document.getElementById("id");
+			for(i=0; i<content.length;i++){
+				content[i].style.display = "none";
+			}
+			document.getElementById("tab_"+id).style.display = "block";
+		});
+	}
+
+});
